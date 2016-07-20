afe482466a4b3e474a27c6923b4277684c4e91b1
zachsis <zachary.hatsis@gmail.com>
2016-07-19 19:38:42 -0600

    added features
    Added additional features and code segments from @dawagz

--- autospoof.py
+++ autospoof.py
@@ -15,16 +15,37 @@ p.add_argument('-p',
                type=int, 
                help='smtp server port', 
                required='True')
-p.add_argument('-t', 
-               '--to', 
+p.add_argument('-r', 
+               '--rcptto', 
                type=str, 
                help='to address', 
                required='True')
-p.add_argument('-F', 
-              '--From', 
+p.add_argument('-m', 
+              '--mailfrom', 
               type=str, 
-              help='from address', 
+              help='MAIL FROM email headers. ' +
+              'please note this email may need to be set ' +
+              'as a valid domain you are sending from to ' +
+              'bypass spf checks', 
               required='True')
+p.add_argument('-d', 
+              '--displayname', 
+              type=str, 
+              help='display from to fool mail clients. ' +
+              'useful if you cant spoof your MAILFROM', 
+              required='True')
+p.add_argument('-d', 
+              '--displayemail', 
+              type=str, 
+              help='display from to fool mail clients. ' +
+              'useful if you cant spoof your MAILFROM', 
+              required='False')
+p.add_argument('-x', 
+              '--xsender, 
+              type=str, 
+              help='rtfm or rtfrfc' +
+              'useful if you cant spoof your MAILFROM', 
+              required='False')
 p.add_argument('-j', 
               '--subject', 
               type=str, 
@@ -34,24 +55,55 @@ p.add_argument('-f',
               '--filename', 
               type=str, 
               help='file attachment')
+p.add_argument('-p', 
+              '--replyto', 
+              type=str, 
+              help='reply to header. rtfrfc')
+p.add_argument('-n', 
+              '--returnpath', 
+              type=str, 
+              help='reply to header. rtfrfc')
 args = p.parse_args()
 
 class mysmtp:
-   def __init__(self, server, port, toEmail, fromEmail, subject):
+   def __init__(self, server, port, rcptto, mailfrom, subject, 
+                displayname, displayemail, xsender, replyto, returnpath):
        self.server = server 
        self.port = port
-       self.toEmail = toEmail
-       self.fromEmail = fromEmail
+       self.rcptto = rcptto
+       self.mailfrom = mailfrom
        self.subject = subject
+       self.displayname = ''
+       self.displayemail = ''
+       self.replyto = ''
+       self.xsender = ''
        self.filename = ''
+       self.returnpath = ''
 
    def send_message(self):
         
        msg = MIMEMultipart('alternative')
-       msg['From'] = self.fromEmail
-       msg['To'] = self.toEmail
+       msg['From'] = self.mailfrom
+       msg['To'] = self.rcptto
        msg['Subject'] = self.subject
-       body = 'sent w/ smtplib and email.mime py libs'
+       
+       if self.displayname:
+         d = "{} \"<{}>\"\r\n".format(self.displayname, self.displayemail)
+       else:
+         d = ''
+       if self.xsender:
+         x = "X-Sender: {} \"<{}>\"\r\n".format(self.displayname, self.displayemail)
+       else:
+         x = ''
+       if self.replyto:
+         rto = "Reply-To: {} \"{}\"\r\n".format(self.displayname, self.replyto)
+       else:
+         rto = ''
+       if self.returnpath:
+         rpat = "Return-Path: {} \"{}\"\r\n".format(self.displayname, self.returnpath)
+       else:
+         rpat = ''
+       body = "{}{}{}{}sent w/ smtplib and email.mime py libs".format(d,x,rto,rpat)
        content = MIMEText(body, 'plain')
        msg.attach(content)
        if self.filename:
